fb0ac808e1d22089106f3fd0183cc8fb04aec32a
GAKS <gaks@cognizant-mobility.com>
2024-06-18 23:01:32 +0200

    Dummy VMs

---
+++ PythonPrac/1.py
@@ -0,0 +1,24 @@
+#print("Hello world")
+
+#skills = ["Talks", 'Comedy', 9, 'Berlin']
+#print(skills[1:4][0])
+
+#Dictionary
+Life = {
+        "Name":"Sagar" , 
+        "Status":"Kinda good", 
+        "Age":31,
+          "City":"berlin", 
+          "Learning":
+                 {"Tech":"AWS",
+                   "Containers":"k8s", 
+                   "CICD":"Jenkins",
+                    "GitOps":
+                    [
+                        "Gitlabs",
+                        "ArgoCD",
+                        "TekTon"
+                    ]
+                 }
+        }
+print(Life)
